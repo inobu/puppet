1a7137d1fc3d6eb0f6e0552fbb5016b056abc71d
inobu <inohkunobuya@gmail.com>
2020-10-01 23:41:28 +0900

    スタンドアロンで動かす？

--- terraform/kube-master.bash
+++ terraform/kube-master.bash
@@ -2,3 +2,7 @@
 sudo rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 sudo yum -y install puppet-agent
 sudo /opt/puppetlabs/bin/puppet resource service puppet ensure=running enable=true
+# puppet module install puppetlabs-kubernetes --version 5.3.0
+# sudo yum -y install docker
+# sudo service docker start
+# docker run --rm -v $(pwd):/mnt --env-file env puppet/kubetool:5.3.0
